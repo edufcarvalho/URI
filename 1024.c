188c900de8545a1b42f048cae35a271c00227bdd
Eduardo Freitas de Carvalho <eduardofc@dcc.ufrj.br>
2020-09-22 00:32:50 -0300

    Studying C/C++ - Day 6 | Coverage 1541 + Improve 1024 solution

--- 1024.c
+++ 1024.c
@@ -2,15 +2,13 @@
 #include <string.h>
 
 char *strrev(char *str) {
-    char *p1, *p2;
-
-    if (!str || !*str)
-        return str;
-        
-    for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; p1++, p2--) {
-        *p1 ^= *p2;
-        *p2 ^= *p1;
-        *p1 ^= *p2;
+    int p1, p2;
+    char tmp;
+    
+    for (p1 = 0, p2 = strlen(str) - 1; p2 > p1; p1++, p2--) {
+        tmp = str[p1];
+        str[p1] = str[p2];
+        str[p2] = tmp;
     }
 
     return str;
@@ -30,11 +28,11 @@ int main(void) {
             if (cript[i] >= 65 && cript[i] <= 90 || cript[i] >= 97 && cript[i] <= 122)
                 cript[i] += 3;
 
-        char* rev = cript;
+        char *rev = cript;
         rev = strrev(rev);
         
         int size = strlen(rev);
-
+        
         for (i = size/2; i < size; i++)
             rev[i] -= 1;
 
---
+++ 1541.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int a, b, c;
+
+    while (scanf("%d", &a), a != 0) {
+        scanf("%d%d", &b, &c);
+        printf("%.0lf\n", floor(sqrt(a*b*100/c)));
+    }
+    return 0;
+}
